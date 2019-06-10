8c5b3aef8b5f34445821e908c3358eb91cea3aed
Max Paulus <mpaulus@login2.is.localnet>
2019-06-11 01:20:47 +0200

    fixed autograd

--- my_autograd.py
+++ my_autograd.py
@@ -3,9 +3,13 @@ import torch
 class CustomGradientScale(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, scale):
-        ctx.save_for_backward(scale)
+        # scale = torch.empty_like(input).fill_(scale) # not sure if this is the best way
+        # ctx.save_for_backward(scale)
+        ctx.scale = scale
         return input
 
     @staticmethod
     def backward(ctx, grad_output):
+        # scale, = ctx.saved_tensors
+        scale = ctx.scale
         return grad_output * scale, None
