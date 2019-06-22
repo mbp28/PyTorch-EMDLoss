c1ce290297cffb56a13e0f7efa85ac41e91e1ce0
Max Paulus <mpaulus@login2.is.localnet>
2019-06-22 09:20:34 +0200

    analyse.py as of June 15

--- analyse.py
+++ analyse.py
@@ -19,6 +19,28 @@ for i in range(0, output.size(0)):
     print("max", output[i].max(0), target[i].max(0))
 
     # CUDA_EMD
+def standardise1(t):
+    s = ((t - t.mean(1)) / t.std(1))
+    return s
+
+def standardise2(t):
+    min, _ = t.min(1)
+    range = t.max(1)[0] - min
+    s = (t- min) / range
+    return s
+
+def standardise3(t):
+    min  = t.min()
+    range = t.max() - min
+    s = (t - min) / range
+    return s
+
+def transform1(t):
+    return torch.sign(t) * torch.log(1+t.abs())
+
+def transform2(t):
+    return torch.sign(t) * t.pow(2)
+
 def run_match(t1, t2, scale):
     n = t1.size(1)
     print('*** Computing cost and match for point clouds with {} points - scaling inputs with {}'.format(n, scale))
@@ -53,19 +75,50 @@ def run_match(t1, t2, scale):
     print('Somewhat matches: \t Total {1:.3g}/{0:.3g} \t [{2:.2%}]'.format(
         n, n_some_match, n_some_match/n))
 
-# for i in range(-10, 10):
-#    scale = 10**i
-#    run_match(pts1_cuda, pts2_cuda, scale)
-print('Output loop')
-for i in range(output.shape[0]):
-    run_match(output[i].reshape(1, output.shape[1], output.shape[2]), output[i].reshape(1, output.shape[1], output.shape[2]), 1)
+a = torch.from_numpy(np.random.randn(1, 10000, 3)).cuda()
+b = a.float() + torch.FloatTensor([10.0,0, 0]).cuda()
+print('numpy dispersion mean')
+run_match(a.float(), b, 1)
 
-print('Target loop')
-for i in range(output.shape[0]):
-    run_match(target[i].reshape(1, output.shape[1], output.shape[2]), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
+#for i in range(output.shape[0]):
+#    run_match(standardise2(output[i].reshape(1, output.shape[1], output.shape[2])), standardise2(target[i].reshape(1, output.shape[1], output.shape[2])), 1)
 
-print('Pairwise loop')
-for i in range(output.shape[0]):
-    run_match(output[i].reshape(1, output.shape[1], output.shape[2]), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
+#for i in range(output.shape[0]):
+#    run_match(standardise3(output[i].reshape(1, output.shape[1], output.shape[2])), standardise3(target[i].reshape(1, output.shape[1], output.shape[2])), 1)
+
+#for i in range(output.shape[0]):
+#    run_match(transform2(output[i].reshape(1, output.shape[1], output.shape[2])), transform2(target[i].reshape(1, output.shape[1], output.shape[2])), 1)
+
+#print('Transform 1 only output')
+#for i in range(output.shape[0]):
+#    run_match(transform1(output[i].reshape(1, output.shape[1], output.shape[2])), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
+
+#print('Transform 2 only output')
+#for i in range(output.shape[0]):
+#    run_match(transform2(output[i].reshape(1, output.shape[1], output.shape[2])), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
+
+#n = 2000
+#torch.manual_seed(0)
+#torch.cuda.manual_seed_all(0)
+#for i in range(output.shape[0]):
+#    perm = torch.randperm(output.size(1))
+#    ix = perm[:n]
+#    run_match(output[i,ix,:].reshape(1, n, output.shape[2]), target[i,ix,:].reshape(1, n, output.shape[2]), 1)
+
+
+#for i in range(-10, 11):
+#   scale = 10**i
+#   run_match(pts1_cuda, pts2_cuda, scale)
+#print('Output loop')
+#for i in range(output.shape[0]):
+#    run_match(output[i].reshape(1, output.shape[1], output.shape[2]), output[i].reshape(1, output.shape[1], output.shape[2]), 1)
+
+#print('Target loop')
+#for i in range(output.shape[0]):
+#    run_match(target[i].reshape(1, output.shape[1], output.shape[2]), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
+
+#print('Pairwise loop')
+#for i in range(output.shape[0]):
+#    run_match(output[i].reshape(1, output.shape[1], output.shape[2]), target[i].reshape(1, output.shape[1], output.shape[2]), 1)
 
 set_trace()
